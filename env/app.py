049b6fa85bd6a11680e4efa69e28448e47c8df83
neevan <neevank997@gmail.com>
2021-07-13 22:07:43 +0530

    new changes

---
+++ env/app.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+app.run()
+
